c2783147ca635cd5df3a035fe57b9abdf9af1706
Nikita Chikunov <chikunov.nikita@sharp-dev.net>
2025-02-19 14:19:43 +0500

    fixed inputs and button

--- src/components/builder.io/InputField.tsx
+++ src/components/builder.io/InputField.tsx
@@ -16,8 +16,7 @@ const InputField: React.FC<InputFieldProps> = ({ label, type, register, name, er
     <div className="flex flex-col mt-5 w-full text-black whitespace-nowrap rounded min-h-[56px] max-md:max-w-full">
       <div className="flex flex-col flex-1 w-full rounded border border-solid border-[#6750A4] max-md:max-w-full">
         <div className="flex flex-1 gap-1 items-start py-1 pl-4 rounded size-full max-md:max-w-full">
-          <div className="flex flex-col flex-1 shrink justify-center py-3 w-full basis-0 min-h-[48px] min-w-[240px] max-md:max-w-full">
-            <label htmlFor={id} className="sr-only">{label}</label>
+        <label htmlFor={id} className="sr-only">{label}</label>
             <input
               {...register(name, { required: `${label} is required` })}
               type={type}
@@ -25,9 +24,8 @@ const InputField: React.FC<InputFieldProps> = ({ label, type, register, name, er
               placeholder={label}
               aria-label={label}
               aria-invalid={error ? "true" : "false"}
-              className="flex-1 shrink self-stretch w-full max-md:max-w-full bg-transparent"
+              className="flex-1 shrink self-stretch w-full max-md:max-w-full bg-transparent outline-none placeholder:text-[#1F1F1F90]"
             />
-          </div>
         </div>
       </div>
       {error && <span role="alert" className="text-red-500 text-sm mt-1">{error.message}</span>}
